af0994c6d69c32c77fea402bc816f9503a1df3ca
MilaKropeczka <milakropeczka@gmail.com>
2025-10-02 11:58:39 +0200

    feat: chart

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,22 @@
+'use client';
+import Chart from '@/components/Chart';
+
+const data = [
+	{ name: 'Jan', Income: 4000, Expenses: 2400 },
+	{ name: 'Feb', Income: 3000, Expenses: 1398 },
+	{ name: 'Mar', Income: 2000, Expenses: 6800 },
+	{ name: 'Apr', Income: 2780, Expenses: 3908 },
+	{ name: 'May', Income: 1890, Expenses: 4800 },
+	{ name: 'Jun', Income: 2390, Expenses: 3800 },
+	{ name: 'Jul', Income: 3490, Expenses: 4300 },
+];
+
 export default function Home() {
 	return (
-		<div className='grid gap-6 w-full h-full grid-cols-1 sm:grid-cols-2 md:grid-cols-4 auto-rows-[150px] p-3'>
-			<div className='bg-white/60 dark:bg-gray-700/95 shadow-xl rounded-3xl sm:col-span-2 sm:row-span-2'></div>
+		<div className='grid gap-6 w-full h-full grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 auto-rows-[150px] p-3'>
+			<div className='bg-white/60 dark:bg-gray-700/95 shadow-xl rounded-3xl sm:col-span-2 row-span-2 flex items-center justify-center p-4'>
+				<Chart data={data} />
+			</div>
 			<div className='bg-white/60 dark:bg-gray-700/95 shadow-xl rounded-3xl'></div>
 			<div className='bg-white/60 dark:bg-gray-700/95 shadow-xl rounded-3xl'></div>
 			<div className='bg-white/60 dark:bg-gray-700/95 shadow-xl rounded-3xl'></div>
---
+++ src/components/Chart.tsx
@@ -0,0 +1,108 @@
+'use client';
+import { useEffect, useState } from 'react';
+import useTheme from '@/lib/useTheme';
+import {
+	XAxis,
+	LabelList,
+	CartesianGrid,
+	ResponsiveContainer,
+	Bar,
+	BarChart,
+	Legend,
+} from 'recharts';
+
+interface ChartData {
+	name: string;
+	Income: number;
+	Expenses: number;
+}
+
+interface Props {
+	data: ChartData[];
+}
+
+export default function Chart({ data }: Props) {
+	const { isDark } = useTheme();
+	const [filteredData, setFilteredData] = useState(data);
+
+	useEffect(() => {
+		const updateData = () => {
+			const width = window.innerWidth;
+			if (width < 359) setFilteredData(data.slice(0, 3));
+			else if (width < 400) setFilteredData(data.slice(0, 5));
+			else setFilteredData(data);
+		};
+
+		updateData();
+		window.addEventListener('resize', updateData);
+		return () => window.removeEventListener('resize', updateData);
+	}, [data]);
+
+	return (
+		<ResponsiveContainer width='100%' height='100%'>
+			<BarChart
+				key={filteredData.length}
+				data={filteredData}
+				barSize={35}
+				barGap={2}>
+				<linearGradient id='incomeGradient' x1='0' y1='0' x2='0' y2='1'>
+					<stop offset='100%' stopColor='#8b5cf6' stopOpacity={0.9} />
+					<stop offset='100%' stopColor='#c4b5fd' stopOpacity={0.9} />
+				</linearGradient>
+				<linearGradient
+					id='expensesGradient'
+					x1='0'
+					y1='0'
+					x2='0'
+					y2='1'>
+					<stop offset='100%' stopColor='#ec4899' stopOpacity={0.9} />
+					<stop offset='100%' stopColor='#f9a8d4' stopOpacity={0.9} />
+				</linearGradient>
+
+				<CartesianGrid
+					strokeDasharray='3 3'
+					strokeOpacity={0.2}
+					className='stroke-black/40 dark:stroke-white/40'
+				/>
+				<XAxis
+					dataKey='name'
+					axisLine={false}
+					tickLine={false}
+					tick={{
+						fill: isDark ? '#ffffff' : '#111827',
+						fontSize: 12,
+					}}
+				/>
+				<Legend
+					formatter={(value) => (
+						<span className='text-black/95 dark:text-white/95'>
+							{value}
+						</span>
+					)}
+				/>
+				<Bar
+					dataKey='Income'
+					fill='url(#incomeGradient)'
+					radius={[10, 10, 0, 0]}>
+					<LabelList
+						dataKey='Income'
+						position='top'
+						fill={isDark ? '#e5e5e5' : '#111827'}
+						fontSize={12}
+					/>
+				</Bar>
+				<Bar
+					dataKey='Expenses'
+					fill='url(#expensesGradient)'
+					radius={[10, 10, 0, 0]}>
+					<LabelList
+						dataKey='Expenses'
+						position='top'
+						fill={isDark ? '#e5e5e5' : '#111827'}
+						fontSize={12}
+					/>
+				</Bar>
+			</BarChart>
+		</ResponsiveContainer>
+	);
+}
