a388d7669d2def93a0dcc69d0920418561d0aff1
MilaKropeczka <milakropeczka@gmail.com>
2025-10-08 12:56:10 +0200

    fix: style HomePage

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,7 +4,7 @@ import BottomNav from '@/components/Navigation/BottomNav';
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
 	return (
-		<div className='flex w-full min-h-screen'>
+		<div className='flex w-full min-h-screen max-w-[1920px] mx-auto'>
 			<Sidebar />
 			<main className='flex-1 overflow-auto'>{children}</main>
 			<BottomNav />
